1253a37aff573f856755dce8433fdabde8f4a4ab
kshiteejm <kshiteejm@rohan.of.wail.wisc.edu>
2017-04-07 15:35:26 -0500

    more query additions

---
+++ ddl-tpch/bin_partitioned/analyze_table.sql
@@ -0,0 +1,8 @@
+analyze table nation compute statistics;
+analyze table region compute statistics;
+analyze table supplier compute statistics;
+analyze table part compute statistics;
+analyze table partsupp compute statistics;
+analyze table customer compute statistics;
+analyze table orders compute statistics;
+analyze table lineitem compute statistics;
